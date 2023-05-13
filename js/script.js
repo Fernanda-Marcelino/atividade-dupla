3cb2041faf17a7e05b050bdb5b30a89ae55d7f7f
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-05-13 20:17:23 -0300

    commit 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canva1');
 const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = 'blue';
     ctx.fillRect (20,20,150,200);
     ctx.strokeRect (20,20,150,200);
