dbe81fc1b597d617b71fb8186a1106459105f85a
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-05-15 16:28:31 -0300

    triangulo e retangulo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-const canvas = document.getElementById('canva1');
-const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'blue';
-    ctx.fillRect (20,20,150,200);
-    ctx.strokeRect (20,20,150,200);
+var canvas = document.getElementById ('canvas1');
+var ctx = canvas.getContext ('2d');
+    
+    
+    //Retângulo
+    ctx.fillStyle = "red";
+    ctx.fillRect (300,50,150,100);
+    ctx.strokeRect (300, 50, 150, 100);
+
+    //Triângulo
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(120,15);
+    ctx.lineTo(55,150);
+    ctx.lineTo(185,150);
+    ctx.fill(); 
+    
+    
