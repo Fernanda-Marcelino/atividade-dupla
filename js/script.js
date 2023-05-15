c9a13387e03c97fd2fcd2008d9d421a178b617e8
Fernanda-Marcelino <fernanda.marcelino2@etec.sp.gov.br>
2023-05-15 17:29:28 -0300

    circulo

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ var ctx = canvas.getContext ('2d');
     
     //Retângulo
     ctx.fillStyle = "red";
-    ctx.fillRect (300,50,150,100);
-    ctx.strokeRect (300, 50, 150, 100);
+    ctx.fillRect(300,50,150,100);
+    ctx.strokeRect(300, 50, 150, 100);
 
     //Triângulo
     ctx.fillStyle = "blue";
@@ -14,5 +14,15 @@ var ctx = canvas.getContext ('2d');
     ctx.lineTo(55,150);
     ctx.lineTo(185,150);
     ctx.fill(); 
+    ctx.stroke();
+
+    //Círculo
+    ctx.fillStyle = "yellow";
+    ctx.beginPath();
+    ctx.arc(605,95,75,0,2 * Math.PI);
+    ctx.moveTo(110,75);
+    ctx.fill()
+    ctx.stroke();
     
-    
+    //Linhas e Curvas (coração)
+    ctx.arc(100,75,50,0*Math.PI,1.5*Math.PI);
